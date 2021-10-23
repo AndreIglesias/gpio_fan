8a37475d076f2743fc2b28b21e75fc1701c2d955
ciglesia <ciglesia@student.42.fr>
2021-10-23 23:38:04 +0200

    indentation

--- gpio_fan.c
+++ gpio_fan.c
@@ -46,46 +46,46 @@ int main(void)
   int ret;
 
   if (OFF_THRESHOLD >= ON_THRESHOLD)
-    {
+  {
       perror("OFF_THRESHOLD must be less than ON_THRESHOLD");
       return (1);
-    }
+  }
   chip = gpiod_chip_open_by_name(CHIPNAME);
   if (!chip)
-    {
+  {
       perror("Open chip failed\n");
       return (1);
-    }
+  }
   line = gpiod_chip_get_line(chip, GPIO_PIN);
   if (!line) {
-    perror("Get line failed\n");
-    gpiod_chip_close(chip);
-    return (1);
+	  perror("Get line failed\n");
+	  gpiod_chip_close(chip);
+	  return (1);
   }
   ret = gpiod_line_request_output(line, CONSUMER, 0);
   if (ret < 0)
-    {
+  {
       perror("Request line as output failed\n");
       gpiod_line_release(line);
       gpiod_chip_close(chip);
       return (1);
-    }
+  }
   while (1)
-    {
-      temp = get_temp();
+  {
+	  temp = get_temp();
       printf("%d > %d\n", temp, ON_THRESHOLD);
       if(temp > ON_THRESHOLD)
-	  ret = gpiod_line_set_value(line, 1);
+		  ret = gpiod_line_set_value(line, 1);
       else if (temp < OFF_THRESHOLD)
-	  ret = gpiod_line_set_value(line, 0);
+		  ret = gpiod_line_set_value(line, 0);
       if (ret < 0)
-	{
-	  perror("Set line output failed\n");
-	  gpiod_line_release(line);
-	  gpiod_chip_close(chip);
-	  return (1);
-	}
+	  {
+		  perror("Set line output failed\n");
+		  gpiod_line_release(line);
+		  gpiod_chip_close(chip);
+		  return (1);
+	  }
       sleep(SLEEP_INTERVAL);
-    }
+  }
   return (0);
 }
